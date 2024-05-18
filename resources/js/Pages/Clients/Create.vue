1095a10c196d425765978d3e8a96ac85a9fb5435
maass <maassi.kloull@gmail.com>
2024-05-18 20:12:06 +0100

    delete required from description field

--- resources/js/Pages/Clients/Create.vue
+++ resources/js/Pages/Clients/Create.vue
@@ -36,7 +36,7 @@
          
             <div class="mt-3">
                 <InputLabel for="description" value="Description(Facultatif)" />
-                <TextInput id="description" type="text" class="mt-1 block w-full" v-model="form.description" required autocomplete="description" />
+                <TextInput id="description" type="text" class="mt-1 block w-full" v-model="form.description"  autocomplete="description" />
                 <InputError class="mt-2" :message="form.errors.name" />
             </div>
 
